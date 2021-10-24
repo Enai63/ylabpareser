dc28b7db53a6f0014526a50cab68ae9e16ec32e4
Enai63 <Toha76@yandex.ru>
2021-10-24 21:31:46 +0400

    Initial commit

---
+++ src/main/java/com/ylab/Main.java
@@ -0,0 +1,9 @@
+package com.ylab;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args[0].equals("-f")) {
+
+        }
+    }
+}
---
+++ src/main/java/com/ylab/handler/Handler.java
@@ -0,0 +1,18 @@
+package com.ylab.handler;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class Handler extends DefaultHandler {
+
+
+    @Override
+    public void startElement(String uri,
+                             String localName,
+                             String qName,
+                             Attributes attributes) throws SAXException {
+
+        
+    }
+}
