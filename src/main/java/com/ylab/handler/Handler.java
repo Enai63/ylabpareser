0e6ad4ad6bc73a1d13df39ed61db411dbd1c3685
enai63 <Toha76@yandex.ru>
2021-10-26 19:23:51 +0400

    add child

--- src/main/java/com/ylab/handler/Handler.java
+++ src/main/java/com/ylab/handler/Handler.java
@@ -1,10 +1,13 @@
 package com.ylab.handler;
 
+import com.ylab.entity.Child;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class Handler extends DefaultHandler {
+
+    private Child child;
 
 
     @Override
@@ -12,7 +15,26 @@ public class Handler extends DefaultHandler {
                              String localName,
                              String qName,
                              Attributes attributes) throws SAXException {
-
+        if(qName.equals("child")) {
+            child = new Child();
+            Boolean isFile = Boolean.parseBoolean(attributes.getValue("isFile"));
+            child.setFile(isFile);
+        }
         
     }
+
+
+    @Override
+    public void endElement(String uri,
+                           String localName,
+                           String qName) throws SAXException {
+
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+    }
+
+
 }
